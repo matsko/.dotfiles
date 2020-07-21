475515f5764e574fd0261ee7d7082f6aed517df7
Matias Niemelä <matias.niemela@ip-192-168-86-172.ec2.internal>
2020-07-21 13:49:30 -0700

    Added `.gitconfig` for shared settings

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ git clone git@github.com:matsko/.dotfiles.git
 cd ~
 ln -s ./.dotfiles/.aliases
 ln -s ./.dotfiles/.vimrc
+ln -s ./.dotfiles/.gitconfig
 mkdir -p .config/nvim
 cd .config/nvim
 ln -s ../../.dotfiles/.vimrc ./init.vim
